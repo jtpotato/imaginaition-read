fcd63577827ecbc3e636448eb7411186cbaa7a42
Joel <58995538+jtpotato@users.noreply.github.com>
2023-05-20 15:20:56 +1000

    better styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,15 @@
 	let inputText = '';
 </script>
 
-<textarea
-	on:input={(event) => {
-		inputText = event.currentTarget.value;
-	}}
-    placeholder="Your notes go here..."
-/>
-
-<Renderer text={inputText}></Renderer>
+<div class="flex flex-row p-4">
+	<textarea
+		on:input={(event) => {
+			inputText = event.currentTarget.value;
+		}}
+		placeholder="Your notes go here..."
+        class="outline-none border-none bg-neutral-800 resize-none rounded-xl w-48 h-24 p-4 text-white"
+	/>
+	<div class="ml-8 w-full">
+		<Renderer text={inputText} />
+	</div>
+</div>
--- src/routes/Renderer.svelte
+++ src/routes/Renderer.svelte
@@ -18,6 +18,9 @@
 	let cursor;
 
 	onMount(() => {
+		cursor.style.height = 16 + 'px';
+		cursor.style.width = 16/2 + 'px';
+
 		let frameLoopID = setInterval(() => {
 			// Chunk the text
 			let completionLength = Math.random() * 10 - 1;
@@ -33,6 +36,9 @@
 					let target = document.getElementById('cursor-target');
 					cursor.style.top = target?.offsetTop + 'px';
 					cursor.style.left = target?.offsetLeft + 'px';
+					let height = target?.offsetHeight ? target?.offsetHeight : 16;
+					cursor.style.height = height + 'px';
+					cursor.style.width = height/2 + 'px';
 				}, 1);
 			}
 		}, 50);
@@ -43,15 +49,26 @@
 	});
 </script>
 
-<!--TODO!!!!!!! SANITISE THIS-->
-<div>{@html displayText}</div>
-<div
-	style={'background-color: black; height: 1em; width: 0.5em; position: absolute; transition: 50ms;'}
-	bind:this={cursor}
-/>
+<div class="bg-neutral-800 p-4 rounded-lg">
+	<!--TODO!!!!!!! SANITISE THIS-->
+	<div class="text-white font-sans">{@html displayText}</div>
+	<div class="cursor bg-white absolute transition-all duration-75" bind:this={cursor} />
+</div>
 
 <style>
-    div {
-        font-family: sans-serif;
-    }
+	@keyframes blink {
+		0% {
+			opacity: 2;
+		}
+		50% {
+			opacity: 0.8;
+		}
+		100% {
+			opacity: 0.9;
+		}
+	}
+	.cursor {
+		animation: blink 1s infinite;
+		box-shadow: 0 0 20px 10px #FF43F1;
+	}
 </style>
