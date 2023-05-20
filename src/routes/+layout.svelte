99223b15a335fe6e704acbf5b14151dd6ef92e7e
Joel <58995538+jtpotato@users.noreply.github.com>
2023-05-20 20:04:56 +1000

    re-add cloudflare analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <svelte:head>
+    <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon={'{"token": "febd7b701285460e8efc9dd16b750369"}'}></script><!-- End Cloudflare Web Analytics -->
 </svelte:head>
 
 <slot />
