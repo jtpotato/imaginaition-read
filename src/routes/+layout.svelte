1106fd45b8a40eb2e1bb7f8da1b49d033f8d6f56
Joel <58995538+jtpotato@users.noreply.github.com>
2023-05-20 18:33:12 +1000

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script>
-    import "./app.css"
+	import './app.css';
 </script>
 
 <svelte:head>
+	<!-- Cloudflare Web Analytics --><!-- Cloudflare Web Analytics -->
+	<script
+		defer
+		src="https://static.cloudflareinsights.com/beacon.min.js"
+		data-cf-beacon={`{"token":"ecbc0a6ac5b34a36b18f98354e6d83e9"}`}
+	></script><!-- End Cloudflare Web Analytics -->
 </svelte:head>
 
 <slot />
