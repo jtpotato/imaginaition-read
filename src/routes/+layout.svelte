57d4698b037fa2d64cb6751397f51c29ce712f7c
Joel <58995538+jtpotato@users.noreply.github.com>
2023-05-20 19:01:27 +1000

    remove analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,6 @@
 </script>
 
 <svelte:head>
-	<!-- Cloudflare Web Analytics --><!-- Cloudflare Web Analytics -->
-	<script
-		defer
-		src="https://static.cloudflareinsights.com/beacon.min.js"
-		data-cf-beacon={`{"token":"ecbc0a6ac5b34a36b18f98354e6d83e9"}`}
-	></script><!-- End Cloudflare Web Analytics -->
 </svelte:head>
 
 <slot />
